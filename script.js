5fa38987fefff7474fc4d06a0528c6cc8ea36829
Jerami Lessard <jeramilessard@gmail.com>
2019-03-03 23:40:15 -0500

    v4.0 List -> array of objects. Methods- adjust object property values

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
-//Object with todo list items.
+//changed to array of objects-todo list items.
 
 
  var todoList = {
-    todos:['item 1', 'item 2', 'item 3'],
+    todos:[],
     //Method created from displayTodos stand-alone function.
     displayTodos: function() {
         console.log('My Todos:', this.todos);
     },
     //Method created from addTodo stand-alone function.
-    addTodo: function(todo) {
-    this.todos.push(todo),
+    addTodo: function(todoText) { 
+    this.todos.push({
+        todoText: todoText,
+        completed: false
+    });
     this.displayTodos();   
     },
     //It should have a method to change a change Todo item to a new value
     //created from changeTodo stand-alone function.
-    changeTodo: function(position, newValue) {
-        this.todos[position] = newValue;
+    //Modified changeTodo to work on todos object -> todoText property.
+    changeTodo: function(position, todoText) {
+       // Old code-> this.todos[position] = newValue;
+       this.todos[position].todoText = todoText;
         this.displayTodos();
     },
     //It should have a method to delete a todo item to a new value
     deleteTodo: function (position) {
         this.todos.splice(position, 1);
         this.displayTodos();
+    },
+
+    //It should have a todoList.toggleCompleted method to modify completed property. **I'm going to try modelling it after changeTodo method.
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed; 
+        //Bang(!)operator to give opposite Boolean value. Remember to grab todo. before trying to change value(completed).
+        this.displayTodos();
     }
+
  };
+
 
  todoList.displayTodos();
 
