5b8aa29d289b9f24153520559e98f5f4099439cc
Jerami Lessard <jeramilessard@gmail.com>
2019-03-15 18:44:23 -0400

    v11[Tested, Cleaned]

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /* This script is a todo list app created during completion of Practical JS */
 // 
 // v11
-// todoList.toggleAll should use forEach
-// view.displayTodos should use forEach
+// todoList.toggleAll should use forEach[Done]
+// view.displayTodos should use forEach[Done]
 
 var todoList = {
     todos:[],
@@ -25,7 +25,6 @@ var todoList = {
     toggleCompleted: function(position) {
         var todo = this.todos[position];
         todo.completed = !todo.completed; 
-        //Bang(!)operator to give opposite Boolean value. Remember to grab todo. before trying to change value(completed).
     },
 
     toggleAll: function() {
@@ -47,16 +46,6 @@ var todoList = {
                 todo.completed = true;
             }
         });
-
-        // [Old code refactored to forEach]if (completedTodos === totalTodos) {
-        //     this.todos.forEach(function(todo) {
-        //         todo.completed = false;
-        //     });
-        // } else {
-        //     this.todos.forEach(function(todo) {
-        //         todo.completed = true;
-        //     });
-        // }      
      }
 
 };
@@ -70,7 +59,7 @@ var handlers = {
         addTodoTextInput.value = '';
         view.displayTodos();
     },
-    changeTodo: function() {        
+    changeTodo: function () {
         var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
         var changeTodoTextInput = document.getElementById('changeTodoTextInput');
         todoList.changeTodo((changeTodoPositionInput.valueAsNumber - 1), changeTodoTextInput.value);
@@ -78,27 +67,25 @@ var handlers = {
         changeTodoTextInput.value = '';
         view.displayTodos();
 
-        //The code/course that I'm following mentions that you need to use .valueAsNumber here. Not sure why since input type = "number". Must ask. Works without? Debug is accurate and predictable.
-        //Not sure if this course will get there but, I think I'd like to force completed=false on changeTodo.
+        //I'd like to force completed=false on changeTodo.
     },
-    deleteTodo: function(position) {
-        todoList.deleteTodo(position); 
-        
+    deleteTodo: function (position) {
+        todoList.deleteTodo(position);
+
         view.displayTodos();
     },
-    toggleCompleted: function() {
+    toggleCompleted: function () {
         var toggleCompletedPosition = document.getElementById('toggleCompletedPositionInput')
         todoList.toggleCompleted((toggleCompletedPosition.valueAsNumber - 1));
         toggleCompletedPosition.value = '';
         view.displayTodos();
     },
-    toggleAll: function() {
+    toggleAll: function () {
         todoList.toggleAll();
         view.displayTodos();
     }     
 };
 
-//displayTodos functionality has been changed to automatically display in HTML, and moved to view
 //It clears the ul then, for each todos, create an li element then add(append) it to the ul.
 var view = {
     displayTodos: function () {
@@ -120,24 +107,6 @@ var view = {
             todoLi.appendChild(this.createDeleteButton());
             todosUl.appendChild(todoLi);
         }, this); 
-        
-        // for (var i = 0; i < todoList.todos.length; i++) {
-        //     var todoLi = document.createElement('li');
-        //     var todo = todoList.todos[i];
-        //     var todoTextWithCompletion = '';
-
-        //     if (todo.completed === true) {
-        //         todoTextWithCompletion = '(x) ' + todo.todoText;
-        //     } else {
-        //         todoTextWithCompletion = '(_) ' + todo.todoText;
-        //     }
-
-        //     todoLi.id = i;
-        //     todoLi.textContent = todoTextWithCompletion
-        //     todoLi.appendChild(this.createDeleteButton());
-        //     todosUl.appendChild(todoLi);
-
-        // }
     },
 
     createDeleteButton: function () {
@@ -153,7 +122,7 @@ var view = {
         todosUl.addEventListener('click', function (event) {
             //Get the element clicked on.
             var elementClicked = event.target;
-
+            //Check if elementClicked is a deleteButton. If so, run deleteTodo.
             if (elementClicked.className === 'deleteButton') {
                 handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
             }
